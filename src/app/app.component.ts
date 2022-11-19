fa6a3d8bc786a8aa74aa4690dc73362a2e2b1663
unknown <huynhbao162000@gmail.com>
2022-11-19 16:45:54 +0700

    hi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UiService } from './services/ui/ui.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Minimus';
+
+  showMenu: boolean = false;
+  darkModeActive: boolean = false;
+
+  constructor(public ui: UiService, public router: Router) {
+  }
+
+  ngOnInit() {
+    this.ui.darkModeState.subscribe((value) => {
+      this.darkModeActive = value;
+    });
+  }
+
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
+  }
+
+  modeToggleSwitch() {
+    this.ui.darkModeState.next(!this.darkModeActive);
+  }
+
 }
